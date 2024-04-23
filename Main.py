119a75d5ce500ac90e115b06d8ad5350eeff6d64
Rizqi-Ahmad-Kurniawan <81918030+Rizqi-Ahmad-Kurniawan@users.noreply.github.com>
2024-04-23 20:09:49 +0700

    Add files via upload
    Frist Purwadhika DTI Batam Data Science Project Upload -> Module 1

---
+++ Main.py
@@ -0,0 +1,124 @@
+from User_Admin import *
+from User_Doctor import * 
+from Auth_Val import *
+
+# Login Page
+# will check the condition if 
+# the script runs directly as the main program 
+
+if __name__ == "__main__":
+    
+    def login_page():
+        print('''
+            \n\t\tPATIENT RECORDS SYSTEM 
+            "May Your Health be Everlasting"
+            
+            Confirm your IDENTITY:
+            Press
+            1 for Admin-access
+            2 for Doctor-access
+            3 to QUIT Program
+            ''')
+        menuChoice = int(input("Your Input: "))
+        return menuChoice
+    
+    def admin_page(id):
+        print(f'''
+            \n\t\tPATIENT RECORDS SYSTEM 
+            "May Your Health be Everlasting"
+            
+            Acessing as ADMIN \"{id}\":
+            Press
+            1 to Display patient data
+            2 to Display doctor data
+            3 to Add patient data
+            4 to Add doctor data
+            5 to Edit patient data (BioData)
+            6 to Edit doctor data
+            7 to Delete patient data
+            8 to Delete doctor data
+            9 to EXIT to Login page
+            10 to QUIT Program
+            ''')
+        menuChoice = int(input("Your Input: "))
+        return menuChoice
+    
+    def doctor_page(id):
+        print(f'''
+            \n\t\tPATIENT RECORDS SYSTEM 
+            "May Your Health be Everlasting"
+            
+            Acessing as DOCTOR \"{id}\":
+            Press
+            1 to Display patient data
+            2 to Edit patient data (MedData)
+            3 to EXIT to Login page
+            4 to QUIT Program
+            ''')
+        menuChoice = int(input("Your Input: "))
+        return menuChoice
+    
+    # MAIN PROGRAM
+    while True:
+        try:
+            menuChoice = login_page()
+            if menuChoice == 1:
+                loginCredential = userAuth_admin()
+                while True:
+                    try:
+                        subMenuChoice_1 = admin_page(loginCredential)
+                        if subMenuChoice_1 == 1:
+                            display_patientData()
+                        elif subMenuChoice_1 == 2:
+                            display_doctorData()
+                        elif subMenuChoice_1 == 3:
+                            add_patientData()
+                        elif subMenuChoice_1 == 4:
+                            add_doctorData()
+                        elif subMenuChoice_1 == 5:
+                            # BioData
+                            edit_patientData_adm()
+                        elif subMenuChoice_1 == 6:
+                            edit_doctorData()
+                        elif subMenuChoice_1 == 7:
+                            del_patientData()
+                        elif subMenuChoice_1 == 8:
+                            del_doctorData()
+                        elif subMenuChoice_1 == 9:
+                            break
+                        elif subMenuChoice_1 == 10:
+                            print(" THANKS FOR LOGGING IN :)))")
+                            exit(0)
+                        else:
+                            print("Please enter the correct number !!!")
+                    except Exception as e:
+                        print("The error is: ", e)
+            elif menuChoice == 2:
+                loginCredential = userAuth_doctor()
+                while True:
+                    try:
+                        subMenuChoice_2 = doctor_page(loginCredential)
+                        if subMenuChoice_2 == 1:
+                            display_patientData()
+                        elif subMenuChoice_2 == 2:
+                            # MedData
+                            edit_patientData_doc()
+                        elif subMenuChoice_2 == 3:
+                            break
+                        elif subMenuChoice_2 == 4:
+                            print(" THANKS FOR LOGGING IN :)))")
+                            exit(0)
+                        else:
+                            print("Please enter the correct number !!!")
+                    except Exception as e:
+                        print("The error is: ", e)
+            elif menuChoice == 3:
+                print(" THANKS FOR LOGGING IN :)))")
+                exit(0)
+            else:
+                print("Please enter the correct number !!!")
+        except Exception as e:
+            print("The error is: ", e)
+
+
+        
---
+++ User_Doctor.py
@@ -0,0 +1,58 @@
+# DOCTOR Functions
+
+from tabulate import tabulate  
+from Data_Class import AllData, DoctorData
+from Data_DB import MainDB
+import pandas as pd
+
+
+# Display MainDB (READ)
+def display_patientData():
+    header = ["Patient ID", "Doctor ID", "Name", "Age", "Gender", "Diagnosis", "Lab Results", "Allergies"]
+
+    if not MainDB:
+        print("\nNo patients present !!!")
+    else:
+        print("\n ===== PATIENT HEALTH RECORDS ===== ")
+        # Creating Iterable Object
+        patients_data = [(index + 1, patient.patientID, patient.doctorID, patient.name, patient.age, patient.gender, patient.diagnosis, patient.labResults, patient.allergies) for index, patient in enumerate(MainDB)]
+        print(tabulate(patients_data, headers=header, tablefmt="fancy_grid"))
+
+# Add (Edit) new Patient data (class MedData) to MainDB (UPDATE)
+def edit_patientData_doc():
+    display_patientData()
+    print("\n ===== EDIT PATIENT DATA (MED) ===== \n")
+    targetPatientID = input("\nEnter Patient ID to be Edited (Med-Data):  ")
+    for patientData in MainDB:
+        if patientData.patientID.lower() == targetPatientID.lower():
+            print(f"\nCurrent Medical Details of the Patient \"{patientData.patientID}\": ")
+            print(" = = = = = = = = = = ")
+            print(f"Doctor Notes: {patientData.docNotes}")
+            print(f"Diagnosis: {patientData.diagnosis}")
+            print(f"Prognosis: {patientData.prognosis}")
+            print(f"Lab Results: {patientData.labResults}")
+            print(f"Allergies: {patientData.allergies}")
+            print(f"Medications: {patientData.medications}")
+            
+            # EDIT phase
+            new_docNotes = input("\nUpdated Doctor Notes: (press enter to keep current): ")
+            patientData.docNotes = new_docNotes if new_docNotes else patientData.docNotes
+            
+            new_diagnosis = input("Updated Diagnosis: (press enter to keep current): ")
+            patientData.diagnosis = new_diagnosis if new_diagnosis else patientData.diagnosis
+            
+            new_prognosis = input("Updated Prognosis: (press enter to keep current): ")
+            patientData.prognosis = new_prognosis if new_prognosis else patientData.prognosis
+            
+            new_labResults = input("Updated Lab Results [format: Avg BPM: x | BP(S/D): x/x | SPO2: x%]: (press enter to keep current): ")
+            patientData.labResults = new_labResults if new_labResults else patientData.labResults
+            
+            new_allergies = input("Updated Allergies: (press enter to keep current): ")
+            patientData.allergies = new_allergies if new_allergies else patientData.allergies
+            
+            new_medications = input("Updated Medications [format: drug_name (quantity_X/day)]: (press enter to keep current): ")
+            patientData.medications = new_medications if new_medications else patientData.medications
+            
+            print(f"\nPatient \"{patientData.patientID}\" Medical Details Updated Successfully")
+            return
+    print("\nPatient not found !!!")
